9721cf802c14d2e251837c2b11645c51c7a483c7
lisenjie757 <1215954303@qq.com>
2023-05-07 19:53:17 +0800

    as

---
+++ eval.sh
@@ -0,0 +1,5 @@
+python eval.py -d voc \
+               -m yolov2 \
+               --img_size 640 \
+               --weight ./weights/voc/yolov2/yolov2_epoch_51_75.34.pth \
+               --root ../data/ 
